9ac40dfd52ff4231370494c1e7cf61928e2102ad
usefAmr <usef.amr5240@gmail.com>
2024-11-10 13:06:53 +0200

    test location using fetch

--- index.js
+++ index.js
@@ -2,50 +2,26 @@ import express from "express";
 import axios from'axios';
 import IpAddr from 'ipaddr.js'
 import ip from 'ip'
+import fetch from "node-fetch";
 
 const app = express();
 
-app.set("trust proxy", true);
-
-const getClientIP = (req) => {
-  let forwardedFor = req.headers['x-forwarded-for'];
-  let ip = forwardedFor ? forwardedFor.split(',')[0] : null;
-  
-  if (!ip) {
-    ip = req.connection.remoteAddress;
-  }
-  
-  return ip;
-};
-
 app.get("/", async (req, res) => {
   // Set endpoint and your access key
-  const clientIp = getClientIP(req);
-  console.log(clientIp)
-  const accessKey = 'e3ad3634-746c-4779-b322-b2c3660717b5';
+  const response = await fetch('https://api.ipify.org?format=json');
+  const data = await response.json();
+  const clientIp = data.ip
+  const accessKey = '4501e4cb-328c-4255-9b49-4f6bfd8b930f';
   const url = `https://apiip.net/api/check?ip=${clientIp}&accessKey=${accessKey}`;
 
-  try {
-    // Make a request and store the response
-    const response = await axios.get(url);
-    console.log(ip)
-    
-    // Check if the response indicates an error
-    if (response.data.success === false) {
-      // If there's an error, log it and send a 400 status code
-      console.error(`Error checking IP: ${JSON.stringify(response.data.message)}`);
-      res.status(400).json({ error: response.data.message });
-    } else {
-      // If successful, log the currency code
-      console.log(`Currency code for IP ${ip}: ${response.data.currency.code}`);
-      res.json({ currencyCode: response.data.currency.code });
-    }
-  } catch (error) {
-    // Log any network errors
-    console.error('Network error:', error.message);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+  const location = await fetch(url);
+
+  // Decode JSON response:
+  const result = await location.json();
+
+  // Output the "code" value inside "currency" object
+  console.log(result);
+  });
 
 app.listen(3000, () => {
   console.log("SERVER RUNNIHG AT PORT 3000");
