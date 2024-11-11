02baed55852bec6a0acd97d0695fde2ef38dad9c
usefAmr <usef.amr5240@gmail.com>
2024-11-11 11:46:57 +0200

    test location 4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import axios from'axios';
+import axios from 'axios'
 import IpAddr from 'ipaddr.js'
 import ip from 'ip'
 import fetch from "node-fetch";
@@ -7,13 +7,27 @@ import fetch from "node-fetch";
 const app = express();
 const response = await fetch('https://api.ipify.org?format=json');
 const data = await response.json();
-const clientIp = data.ip
+
+app.use((req, res, next) => {
+  req.clientIp = req.headers['x-forwarded-for'] || req.ip;
+  next();
+});
 
 app.get("/", async (req, res) => {
   // Set endpoint and your access key
-  
+  const clientIp = req.clientIp;
+    
+  console.log(`Client IP: ${clientIp}`);
+
+    // Example: Send request to check the client's public IP
+  const response = await axios.get('https://api.ipify.org?format=json');
+  const publicIp = response.data.ip;
+
+  console.log(`Public IP: ${publicIp}`);
+
+  res.send(`Client IP: ${clientIp}, Public IP: ${publicIp}`);
   const accessKey = '4501e4cb-328c-4255-9b49-4f6bfd8b930f';
-  const url = `https://apiip.net/api/check?ip=${clientIp}&accessKey=${accessKey}`;
+  const url = `https://apiip.net/api/check?ip=${publicIp}&accessKey=${accessKey}`;
 
   const location = await fetch(url);
 
